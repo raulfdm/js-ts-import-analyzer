802fd9d7f5057ca94714c8669e3783941aeaaa35
Raul <melo.raulf@gmail.com>
2020-02-14 08:04:23 +0100

    add pretty-error

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,22 @@
 import fs from "fs";
+import PrettyError from "pretty-error";
 
 import { config } from "./config";
 import { analyzeProjects } from "./analyzer";
 import { collectFilesToAnalyzePerProject } from "./collect";
 
+console.time("Time");
+console.log("Generating report...");
+
 Promise.resolve()
   .then(collectFilesToAnalyzePerProject)
   .then(analyzeProjects)
-  .then(csvContent => fs.writeFileSync(config.csv.path, csvContent));
+  .then(csvContent => fs.writeFileSync(config.csv.path, csvContent))
+  .then(function() {
+    console.log("Report generated sucessfully");
+    console.timeEnd("Time");
+  })
+  .catch(e => {
+    const pe = new PrettyError();
+    console.log(pe.render(e));
+  });
